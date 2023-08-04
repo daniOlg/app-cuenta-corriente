d065eb5c2a0838e84f2bc543bff6598bfc1528f5
Daniel Olguín Martínez <dani.olg@hotmail.com>
2023-08-04 14:39:28 -0400

    feat: se exporta programa principal

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
-import cuentas.AdministradorCuentas;
 import cuentas.CuentaCorriente;
 
-import java.util.Scanner;
+
+import static cuentas.AdministradorCuentas.programaUsuario;
 
 public class Main {
     public static void main(String[] args) {
@@ -40,94 +40,7 @@ public class Main {
         cuentaCorrienteRandom.abonar(59990);    // +59.990
         cuentaCorrienteRandom.cargar(160000);   // -160.000
 
-        // creamos un mini programa para probar funcionalidades (sin codigo muy legible solo experimental)
-        miniPrograma();
-    }
-
-    public static void miniPrograma() {
-        final int LISTAR_CUENTAS = 1, VER_DETALLE = 2, CREAR_CUENTA = 3, ABONAR = 4, CARGAR = 5, SALIR = 6;
-
-        // creamos un loop infinito
-        while(true) {
-            System.out.println("--------------------------------------------------------");
-            System.out.println("Que desea hacer?");
-            System.out.println("1. Listar cuentas existentes");
-            System.out.println("2. Ver detalle de una cuenta (codigo necesario)");
-            System.out.println("3. Crear una cuenta");
-            System.out.println("4. Abonar (codigo necesario)");
-            System.out.println("5. Cargar (codigo necesario)");
-            System.out.println("6. Salir");
-            System.out.println();
-            System.out.print("Seleccion: ");
-
-            Scanner scanner = new Scanner(System.in);
-            int seleccionMenu = scanner.nextInt();
-
-            // menu handling
-            if(seleccionMenu == LISTAR_CUENTAS) {
-                System.out.println("--------------------------------------------------------");
-                System.out.println("Cuentas existentes:");
-                for(CuentaCorriente cuenta : AdministradorCuentas.cuentas) {
-                    System.out.println("- " + cuenta.datosBasicosToString());
-                }
-            } else if(seleccionMenu == VER_DETALLE) {
-                System.out.print("Ingrese el numero de la cuenta: ");
-                int numero = scanner.nextInt();
-                for(CuentaCorriente cuenta : AdministradorCuentas.cuentas) {
-                    if(cuenta.getNumero() == numero) {
-                        System.out.println(cuenta);
-                    }
-                }
-            } else if(seleccionMenu == CREAR_CUENTA) {
-                System.out.print("Ingrese el nombre del titular: ");
-                scanner.nextLine();
-                String titular = scanner.nextLine();
-                CuentaCorriente nuevaCuenta = new CuentaCorriente(titular);
-                System.out.println("Cuenta creada con los siguientes datos:");
-                System.out.println(nuevaCuenta);
-            } else if(seleccionMenu == ABONAR) {
-                System.out.print("Ingrese el numero de la cuenta: ");
-                int numero = scanner.nextInt();
-                CuentaCorriente seleccionada = null;
-                for(CuentaCorriente cuenta : AdministradorCuentas.cuentas) {
-                    if(cuenta.getNumero() == numero) {
-                        seleccionada = cuenta;
-                    }
-                }
-
-                if(seleccionada != null) {
-                    System.out.println("Se ha encontrado la cuenta, el saldo actual es de: $" + seleccionada.getSaldo());
-                    System.out.print("Cuanto desea abonar?: ");
-                    float cantidad = scanner.nextFloat();
-                    seleccionada.abonar(cantidad);
-                    System.out.println("El nuevo saldo es de: " + seleccionada.getSaldo());
-                } else {
-                    System.out.println("No se ha encontrado la cuenta");
-                }
-            } else if(seleccionMenu == CARGAR) {
-                System.out.print("Ingrese el numero de la cuenta: ");
-                int numero = scanner.nextInt();
-                CuentaCorriente seleccionada = null;
-                for(CuentaCorriente cuenta : AdministradorCuentas.cuentas) {
-                    if(cuenta.getNumero() == numero) {
-                        seleccionada = cuenta;
-                    }
-                }
-
-                if(seleccionada != null) {
-                    System.out.println("Se ha encontrado la cuenta, el saldo actual es de: $" + seleccionada.getSaldo());
-                    System.out.print("Cuanto desea cargar?: ");
-                    float cantidad = scanner.nextFloat();
-                    seleccionada.cargar(cantidad);
-                    System.out.println("El nuevo saldo es de: " + seleccionada.getSaldo());
-                } else {
-                    System.out.println("No se ha encontrado la cuenta");
-                }
-            } else if(seleccionMenu == SALIR) {
-                System.exit(0);
-            } else {
-                System.out.println("El numero ingresado no es valido, intente nuevamente.\n");
-            }
-        }
+        // creamos un mini programa para probar funcionalidades
+        programaUsuario();
     }
 }

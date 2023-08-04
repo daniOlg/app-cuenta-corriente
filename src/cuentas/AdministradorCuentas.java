a453fe587d483c13976d41536fb397b9b1112f56
Daniel Olguín Martínez <dani.olg@hotmail.com>
2023-08-04 14:16:06 -0400

    feat: se mueven funcionalidades a otra clase

---
+++ src/cuentas/AdministradorCuentas.java
@@ -0,0 +1,9 @@
+package cuentas;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class AdministradorCuentas {
+    public static ArrayList<CuentaCorriente> cuentas = new ArrayList<>();
+    public static HashSet<Integer> identificadoresOcupados = new HashSet<>();
+}

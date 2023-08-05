cd8623bd2ebaf772996600ca8c5b5316af0b4f96
Daniel Olguín Martínez <dani.olg@hotmail.com>
2023-08-05 14:43:48 -0400

    feat: modificaciones varias

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,35 @@
+import admin.AdministradorCuentas;
 import cuentas.CuentaCorriente;
-
-
-import static cuentas.AdministradorCuentas.programaUsuario;
+import static admin.AdministradorCuentas.programaUsuario;
 
 public class Main {
     public static void main(String[] args) {
         // Generar datos de prueba
-        CuentaCorriente cuentaCorrienteDanielIgnacio = new CuentaCorriente("Daniel Ignacio");
+        CuentaCorriente cuentaCorrienteDaniel = new CuentaCorriente("Daniel");
 
-        // Probar mas de 10 movimientos
-        cuentaCorrienteDanielIgnacio.abonar( 150000);   // +150.000     => 150.000
+        // Probar más de 10 movimientos
+        cuentaCorrienteDaniel.abonar( 150000);   // +150.000     => 150.000
 
         // aquí se comprueba que no baja de 0
         // a su vez el movimiento queda registrado como -150.000 ya que el
         // usuario no tiene saldo suficiente para realizar el cargo de 200.000
-        cuentaCorrienteDanielIgnacio.cargar(200000);    // -200.000     => 0
+        cuentaCorrienteDaniel.cargar(200000);    // -200.000     => 0
 
-        cuentaCorrienteDanielIgnacio.abonar(1000000);   // +1.000.000   => 1.000.000
-        cuentaCorrienteDanielIgnacio.abonar( 150000);   // +150.000     => 1.150.000
-        cuentaCorrienteDanielIgnacio.cargar(200000);    // -200.000     => 950.000
+        cuentaCorrienteDaniel.abonar(1000000);   // +1.000.000   => 1.000.000
+        cuentaCorrienteDaniel.abonar( 150000);   // +150.000     => 1.150.000
+        cuentaCorrienteDaniel.cargar(200000);    // -200.000     => 950.000
 
         // últimos 10 movimientos reflejados en el print
-        cuentaCorrienteDanielIgnacio.cargar(100000);    // -100.000     => 850.000
-        cuentaCorrienteDanielIgnacio.abonar( 500000);   // +500.000     => 1.350.000
-        cuentaCorrienteDanielIgnacio.abonar(59990);     // +59.990      => 1.409.990
-        cuentaCorrienteDanielIgnacio.cargar(100000);    // -100.000     => 1.309.990
-        cuentaCorrienteDanielIgnacio.cargar(2000);      // -2.000       => 1.307.990
-        cuentaCorrienteDanielIgnacio.abonar(159990);    // +159.990     => 1.467.980
-        cuentaCorrienteDanielIgnacio.abonar( 399000);   // +399.000     => 1.866.980
-        cuentaCorrienteDanielIgnacio.cargar(200000);    // -200.000     => 1.666.980
-        cuentaCorrienteDanielIgnacio.cargar(100000);    // -100.000     => 1.566.980
-        cuentaCorrienteDanielIgnacio.abonar(700000);    // +700.000     => 2.266.980
+        cuentaCorrienteDaniel.cargar(100000);    // -100.000     => 850.000
+        cuentaCorrienteDaniel.abonar( 500000);   // +500.000     => 1.350.000
+        cuentaCorrienteDaniel.abonar(59990);     // +59.990      => 1.409.990
+        cuentaCorrienteDaniel.cargar(100000);    // -100.000     => 1.309.990
+        cuentaCorrienteDaniel.cargar(2000);      // -2.000       => 1.307.990
+        cuentaCorrienteDaniel.abonar(159990);    // +159.990     => 1.467.980
+        cuentaCorrienteDaniel.abonar( 399000);   // +399.000     => 1.866.980
+        cuentaCorrienteDaniel.cargar(200000);    // -200.000     => 1.666.980
+        cuentaCorrienteDaniel.cargar(100000);    // -100.000     => 1.566.980
+        cuentaCorrienteDaniel.abonar(700000);    // +700.000     => 2.266.980
 
         CuentaCorriente cuentaCorrienteRandom = new CuentaCorriente("Random Person");
 
@@ -41,7 +40,8 @@ public class Main {
         cuentaCorrienteRandom.abonar(59990);    // +59.990
         cuentaCorrienteRandom.cargar(160000);   // -160.000
 
-        // creamos un mini programa para probar funcionalidades
+
+        // creamos un programa para probar funcionalidades => en esta función se usan try/catch
         programaUsuario();
     }
 }
